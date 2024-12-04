296fbb8de160ebadffa99ba34bc282612e11b894
Brian Best <brianbest@Brians-MacBook-Air.local>
2024-12-03 21:41:55 -0600

    feature: contact form and dark mode theme

---
+++ app/api/send/route.ts
@@ -0,0 +1,28 @@
+import { EmailTemplate } from "@/components/contact-form/email-template";
+import { Resend } from "resend";
+import * as React from "react";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST(request: Request) {
+  try {
+    const { email, message } = await request.json();
+    const { data, error } = await resend.emails.send({
+      from: "Acme <onboarding@resend.dev>",
+      to: ["hello@brianbest.dev"],
+      subject: "New brianbest.dev inquiry 🎉",
+      react: EmailTemplate({
+        firstName: email,
+        message: message,
+      }) as React.ReactElement,
+    });
+
+    if (error) {
+      return Response.json({ error }, { status: 500 });
+    }
+
+    return Response.json({ data });
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
