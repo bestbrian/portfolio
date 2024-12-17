67c259731fb0feae98f143f6780128f2c918452b
Brian Best <contact@brian.best>
2024-12-17 13:42:58 -0500

    chore: content updates

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { InputForm } from "@/components/contact-form";
+import { ContactForm } from "@/components/contact-form";
 import { Badge } from "@/components/ui/badge";
 import { SplitLayout } from "@/components/split-layout";
 import { EventItem } from "@/components/event-item";
@@ -24,59 +24,66 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <div>
-        <h2 className="border-solid border-2 border-sky-500 font-subway font-black mt-8 mb-4 text-3xl dark:text-yellow-200 {styles.TorontoSubway} font-medium">
-          WELCOME, BRIAN BEST
+        <h2 className="font-subway font-black mt-8 mb-4 text-3xl dark:text-yellow-200 {styles.TorontoSubway} font-medium">
+          HI, MY NAME IS BRIAN AND I AM THE BEST
         </h2>
         <section className="flex flex-col gap-12">
-          <SplitLayout section="Bio">
+          <SplitLayout section="BIO">
             <div className="p-4">
-              Customize the number of columns, rows, and gaps to fit your needs.
               <p>
-                2. Click the square with + sign to add a new element to the
-                grid. 3. Resize the DIV using the handle in the bottom right
-                corner.
+                Seasoned tech professional with extensive experience in FinTech
+                and SaaS startups, specializing in scalable digital solutions
+                and agile team leadership.
+                <br />
+                <br />
+                With a strong background in Scrum, I’ve led teams through sprint
+                planning, backlog refinement, and daily stand-ups, ensuring
+                timely delivery and alignment with business goals. My expertise
+                spans product development, user experience enhancement, and
+                marketing automation.
+                <br />
+                <br />
+                Passionate about leveraging data to drive growth and streamline
+                processes, I’m excited to collaborate with innovative teams.
               </p>
-              <p>4. Drag and drop the DIV to reposition it as desired.</p>
-              5. Finally, copy the generated HTML code and paste it into your
-              project.
             </div>
           </SplitLayout>
 
-          <SplitLayout section="Experience">
+          <SplitLayout section="EXPERIENCE">
             <EventItem
               title="Senior Product Manager"
               body="Musth"
-              timeline="2022 –– 2024"
+              timeline="2022 – 2024"
               image={musthLogo}
             />
             <EventItem
               title="Software Engineer"
               body="Better"
-              timeline="2020 –– 2022"
+              timeline="2020 – 2022"
               image={betterLogo}
             />
             <EventItem
               title="Co-Founder & Chief Operating Officer"
               body="BrandLoyal"
-              timeline="2014 –– 2022"
+              timeline="2014 – 2022"
               image={brandloyalLogo}
             />
             <EventItem
               title="Software Engineer"
               body="Vroom"
-              timeline="2018 –– 2020"
+              timeline="2018 – 2020"
               image={vroomLogo}
             />
           </SplitLayout>
 
-          <SplitLayout section="Skills">
+          <SplitLayout section="SKILLS">
             <BadgeGrid title="Product Management" badges={ProductSkills} />
             <BadgeGrid title="Technical" badges={TechnicalSkills} />
             <BadgeGrid title="Collaboration" badges={CollabSkills} />
             <BadgeGrid title="Marketing" badges={MarketingSkills} />
           </SplitLayout>
 
-          <SplitLayout section="Education">
+          <SplitLayout section="EDUCATION">
             <EventItem
               title="Bachelor of Human Resources Management"
               body="York University, Toronto"
@@ -91,15 +98,17 @@ export default function Home() {
             />
           </SplitLayout>
 
-          <SplitLayout section="Software">
+          <SplitLayout section="SOFTWARE">
             <ToolGrid tools={SoftwareTools} />
           </SplitLayout>
 
-          <SplitLayout section="Tech">
+          <SplitLayout section="TECH">
             <ToolGrid tools={TechTools} />
           </SplitLayout>
         </section>
-        <InputForm />
+        <section className="mt-16">
+          <ContactForm />
+        </section>
       </div>
     </main>
   );
