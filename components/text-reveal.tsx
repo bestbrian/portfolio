1fff071a93afd4284f38c9623173fda6424224a3
Brian Best <contact@brian.best>
2025-01-15 00:43:20 -0500

    update: experience section

--- components/text-reveal.tsx
+++ components/text-reveal.tsx
@@ -20,7 +20,7 @@ const TextRevealByWord: FC<Props> = ({ paragraph, className, children }) => {
 
   return (
     <div ref={targetRef} className={cn("relative z-0 h-[200vh]", className)}>
-      <div className="sticky top-0 mx-auto flex h-[50%] w-full flex-col items-center bg-transparent px-[1rem] py-[5rem]">
+      <div className="sticky top-0 mx-auto flex h-[36%] w-full flex-col items-center bg-transparent pt-10">
         <p className="max-w-3xl flex flex-wrap p-5 text-2xl font-bold text-black/20 dark:text-white/20 md:p-8 md:text-3xl lg:p-10 lg:text-5xl">
           {words.map((word, i) => {
             const start = i / words.length;
